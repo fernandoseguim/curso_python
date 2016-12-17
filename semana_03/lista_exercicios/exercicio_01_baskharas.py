16e9f7eb07945dd01719d483fdf0ec980300bf22
Fernando Seguim <fernando.seguim@gmail.com>
2016-12-17 15:04:42 -0200

    semana 3, lista de exercicios (baskara, paridade, fizz, buzz, fizzbuzz, ordenacao, distancia)

--- semana_03/lista_exercicios/exercicio_01_baskharas.py
+++ semana_03/lista_exercicios/exercicio_01_baskharas.py
@@ -9,15 +9,19 @@ def main():
     delta = b*b - (4 * a * c)
 
     if delta < 0:
-        print("Esta equação não possui raízes reais!")
-
-    elif delta == 0:
-        x1 = (-b + math.sqrt(delta)) / (2 * a)
-        print("Esta equação possui apenas a raíz %.2f" % (x1))
+        print("esta equação não possui raízes reais")
 
     else:
         x1 = (-b + math.sqrt(delta)) / (2 * a)
         x2 = (-b - math.sqrt(delta)) / (2 * a)
-        print("Esta equação possui as raízes %.2f e %.2f" % (x1,x2))
+
+        if x1 == x2:
+            print("a raiz desta equação é ", x1)
+        else:
+            if x1 > x2:
+                aux = x1
+                x1 = x2
+                x2 = aux
+            print("as raízes da equação são ", x1, " e " ,x2)
 
 main()
